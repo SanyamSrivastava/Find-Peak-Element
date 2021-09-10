868143bedc7d78d18aeb313f9fce552a1bc6ffb0
DevSan <55429363+SanyamSrivastava@users.noreply.github.com>
2021-09-10 18:45:40 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -4,14 +4,14 @@ class Solution{
     
     static int findPeakElement(int[] nums) {
         int lo = 0, hi = nums.length-1;
-        if(nums.length==1)
+        if(nums.length==1) //If single element then return the 0 index
             return 0;
-        if(nums[1] < nums[0])
+        if(nums[1] < nums[0]) //If leftmost element is peak
             return 0;
-        if(nums[nums.length-1] > nums[nums.length-2])
+        if(nums[nums.length-1] > nums[nums.length-2]) //If rightmost element is peak
             return nums.length-1;
         while(lo < hi){
-            int mid = lo + (hi - lo)/2;
+            int mid = lo + (hi - lo)/2; //Prevention of INTEGER_OVERFLOW
             if( (mid == 0 || nums[mid-1] <= nums[mid]) && (mid == nums.length-1 || nums[mid+1] <= nums[mid]))
                 return mid;
             else{
